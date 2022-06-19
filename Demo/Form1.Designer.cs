50c8ade4acd64513b35a83eec5fe617a9b7949bc
Smirnova199.ru@yandex.ru <Smirnova199.ru@yandex.ru>
2022-06-19 19:45:04 +0300

    Knopkadof1

--- Demo/Form1.Designer.cs
+++ Demo/Form1.Designer.cs
@@ -28,14 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.demaSUDataSet = new Demo.DemaSUDataSet();
+            this.tkaniBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tkaniTableAdapter = new Demo.DemaSUDataSetTableAdapters.TkaniTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.demaSUDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tkaniBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.tipDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tkaniBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(94, 57);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
@@ -50,16 +66,72 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Привет";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(245, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 230);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 268);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // demaSUDataSet
+            // 
+            this.demaSUDataSet.DataSetName = "DemaSUDataSet";
+            this.demaSUDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tkaniBindingSource
+            // 
+            this.tkaniBindingSource.DataMember = "Tkani";
+            this.tkaniBindingSource.DataSource = this.demaSUDataSet;
+            // 
+            // tkaniTableAdapter
+            // 
+            this.tkaniTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // tipDataGridViewTextBoxColumn
+            // 
+            this.tipDataGridViewTextBoxColumn.DataPropertyName = "tip";
+            this.tipDataGridViewTextBoxColumn.HeaderText = "tip";
+            this.tipDataGridViewTextBoxColumn.Name = "tipDataGridViewTextBoxColumn";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.demaSUDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tkaniBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +141,14 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private DemaSUDataSet demaSUDataSet;
+        private System.Windows.Forms.BindingSource tkaniBindingSource;
+        private DemaSUDataSetTableAdapters.TkaniTableAdapter tkaniTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tipDataGridViewTextBoxColumn;
     }
 }
 
--- Demo/Form1.cs
+++ Demo/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,55 @@ namespace Demo
 {
     public partial class Form1 : Form
     {
+        string ConnStr = @"Data Source=ngknn.ru;Initial Catalog=DemaSU;Persist Security Info=True;User ID=44П;Password=***********";
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            FillGrups();
+
+        }
+
+        public void FillGrups()
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "demaSUDataSet.Tkani". При необходимости она может быть перемещена или удалена.
+            this.tkaniTableAdapter.Fill(this.demaSUDataSet.Tkani);
+
+            //string SqlText = "SELECT * FROM [Tkani]";
+            //SqlDataAdapter da = new SqlDataAdapter(SqlText, ConnStr);
+            //DataSet ds = new DataSet();
+            //da.Fill(ds, "[Tkani]");
+            //dataGridView1.DataSource = ds.Tables["[Tkani]"].DefaultView;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConnStr);
+            // Выполняем запрос к базе данных
+            con.Open();//открываем соединение
+            string SqlText = "INSERT INTO [Tkani] ([id],[tip])" + " VALUES(@id, @tip)";
+            SqlCommand dbCommand = new SqlCommand(SqlText, con);//команда
+            dbCommand.Parameters.AddWithValue("@id", textBox1.Text);
+            dbCommand.Parameters.AddWithValue("@tip", textBox2.Text);
+
+            if (dbCommand.ExecuteNonQuery() != 1)
+
+                MessageBox.Show("Ошибка выполнения запроса! Данные не добавлены!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            else
+
+                MessageBox.Show("Данные добавлены!", "Сообщение!", MessageBoxButtons.OK);
+            FillGrups();
+
+            // очистка данных из тест полей
+            textBox1.Clear();
+            textBox2.Clear();
+
+        }
     }
-}
+ }
+
